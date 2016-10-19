95c12f035d90a1597d503a71597d8ed05575f4e6
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2016-10-19 17:19:03 -0400

    js changes

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -16,6 +16,10 @@ module.exports = function(app){
 		res.sendFile(path.join(__dirname, '../public/survey.html'));
 	});
 
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
+
 	// If no matching  - default to home
 	app.use(function(req, res){
 		res.sendFile(path.join(__dirname, '../public/home.html'));
